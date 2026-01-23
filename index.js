8e185f56a72e9b3b88c8b69cffff8935c7800c18
Sejal Gupta <sejalguptaofficial11@gmail.com>
2026-01-23 23:53:46 +0530

    Adding Google fonts

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// let firstName = "Sejal";
+// console.log("Hello " + firstName );
+// console.log("Namaste " + firstName);
+// console.log("Kiwein aan singh " + firstName);
+// function isLegal(age) {
+    
+// }
+
+
+
+for(let i = 1; i <= 100; i++)
+{
+    if(age >= 18) console.log('Age is legal');
+    else console.log("Age is illegal");
+}
+
+
+
+// isLegal(23);
+// isLegal(2);
