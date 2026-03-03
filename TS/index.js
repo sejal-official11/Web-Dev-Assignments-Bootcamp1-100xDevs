975b19948f163041a1ca136767b8a5d37c934f78
Sejal Gupta <sejalguptaofficial11@gmail.com>
2026-03-03 17:57:02 +0530

    TS

---
+++ TS/index.js
@@ -0,0 +1,11 @@
+"use strict";
+// let x : string | number = "HI";
+// console.log(x);
+// x = 2;
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(firstName) {
+    console.log("Hello " + firstName);
+}
+let firstName = "Sejal";
+greet(firstName);
+//# sourceMappingURL=index.js.map
--- TS/index.ts
+++ TS/index.ts
@@ -1,2 +1,39 @@
-let x : number = "HI";
-console.log(x);
+// let x : string | number = "HI";
+// console.log(x);
+// x = 2;
+
+
+
+function greet(firstName: string) {
+    console.log("Hello " + firstName);
+    
+}
+
+
+let firstName = "Sejal"
+greet(firstName);
+
+
+let p : any = "Sejal";
+p = 9;
+p = {j: 100}
+
+
+function sum(a: number, b : number) {
+    console.log(a+b);
+}
+
+sum(2, 3);
+
+
+function isAdult(age: number) {
+    if(age > 18) console.log(true);
+    else console.log(false);
+}
+
+
+
+isAdult(78);
+
+
+
