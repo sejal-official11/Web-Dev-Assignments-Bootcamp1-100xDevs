6a6d9652444f8d2af4ff7a33aec34b619d86e2fd
Sejal Gupta <sejalguptaofficial11@gmail.com>
2026-03-17 09:54:11 +0530

    Adding frontend backend

--- Backend/Authentication_Todo/Backend/index.js
+++ Backend/Authentication_Todo/Backend/index.js
@@ -1,6 +1,9 @@
-import express from "express";
+const express = require('express');
 
 const app = express();
+
+
+app.use(express.json());
 
 const notes = [];
 
@@ -17,6 +20,11 @@ app.get("/notes", (req, res) => {
      res.json({
           notes
      })
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile("E:/The-new-old-tech-skills-by-Sejal-2026/Web-Dev-Assignments-Bootcamp1-100xDevs/Backend/Authentication_Todo/Frontend/index.html")
 })
 
 
